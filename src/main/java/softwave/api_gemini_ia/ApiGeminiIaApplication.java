74b570c694f1dddc9a3dfecbfd8aace15227a001
letfonsecaa <leticia.santos@sptech.school>
2025-10-07 21:04:13 -0300

    estrutura do projeto

---
+++ src/main/java/softwave/api_gemini_ia/ApiGeminiIaApplication.java
@@ -0,0 +1,13 @@
+package softwave.api_gemini_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGeminiIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGeminiIaApplication.class, args);
+	}
+
+}
